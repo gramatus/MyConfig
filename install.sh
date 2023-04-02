ad5b5db8315e496367e43e5e050e8561a159a5c5
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-02 19:30:25 +0200

    Try to fix packersync bootstrap

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 cp -a .config/. ~/.config
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' # See https://github.com/wbthomason/packer.nvim/issues/502
