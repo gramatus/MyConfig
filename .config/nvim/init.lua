b7faaa5f12dacd93288902f11878ee49ab850f2a
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-03 14:12:33 +0200

    Added support for "fake clipboard" in codespaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,21 @@
 require("torgst")
+-- Create a "fake clipboard", this will allow copying internally in codespaces running in the web using VIM copy mode
+vim.cmd[[
+  if getenv("CODESPACES")==?"true"
+      echo "Setting fake clipboard for Codespaces"
+      let g:clipboard = {
+    \   'name': 'FileClipboard',
+    \   'copy': {
+    \      '+': ['tee', '/tmp/clipboard_plus.txt'],
+    \      '*': ['tee', '/tmp/clipboard_star.txt'],
+    \    },
+    \   'paste': {
+    \      '+': 'cat /tmp/clipboard_plus.txt',
+    \      '*': 'cat /tmp/clipboard_star.txt',
+    \   },
+    \   'cache_enabled': 0,
+    \ }
+  else
+    echo "NOT CS"
+  endif
+]]
