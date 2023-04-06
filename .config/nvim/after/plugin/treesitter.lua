21a9ac59fdaa2521fdaa19f952e91e6f66fb1de2
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-06 14:34:07 +0200

    Fix treesitter errors for vim commands

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -5,7 +5,7 @@ else
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
 
-  ensure_installed = { "javascript", "typescript", "c_sharp", "lua", "json", "jsonc", "terraform", "vue", "sql", "yaml", "regex", "markdown", "markdown_inline" },
+  ensure_installed = { "javascript", "typescript", "c_sharp", "lua", "vim", "json", "jsonc", "terraform", "vue", "sql", "yaml", "regex", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
