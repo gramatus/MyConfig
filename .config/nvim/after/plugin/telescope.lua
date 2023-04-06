efd1a32514e6d6184cec0717f6b97347085b5f87
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-06 19:33:26 +0200

    Added support for hidden files in Telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,6 @@
+require('telescope').setup {
+	file_ignore_patterns = { "^./.git/"},
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- .config/nvim/lua/torgst/remap.lua
+++ .config/nvim/lua/torgst/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<leader>ph", "<cmd>Telescope find_files hidden=true<cr>")
+
 vim.keymap.set("n", "<leader>p", "<nop>")
 vim.keymap.set("n", "<C-k><C-d>", vim.cmd.Format)
 -- TODO: Look at these suggestions: https://alpha2phi.medium.com/neovim-for-beginners-key-mappings-and-whichkey-31dbf58f9f87
