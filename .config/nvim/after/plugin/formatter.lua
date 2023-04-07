4adeb4264c20e799393faa3748b9d80070a7fbe5
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-07 17:05:56 +0200

    Setup formatter.nvim (initial setup)

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -1,16 +1,14 @@
+-- https://github.com/mhartington/formatter.nvim
 require('formatter').setup({
-    logging = false,
-    filetype = {
-      javascript = {
-          -- prettierd
-         function()
-            return {
-              exe = "prettierd",
-              args = {vim.api.nvim_buf_get_name(0)},
-              stdin = true
-            }
-          end
-      },
-      -- other formatters ...
-    }
-  })
+  logging = false,
+  filetype = {
+    lua={ require("formatter.filetypes.lua").stylua },
+    javascript = { require("formatter.filetypes.javascript").prettierd },
+    typescript = { require("formatter.filetypes.javascript").prettierd },
+    vue = { require("formatter.defaults.prettierd") },
+    cs = { require("formatter.filetypes.cs").dotnetformat },
+    css = { require("formatter.filetypes.css").prettierd },
+    json = { require("formatter.filetypes.json").prettierd },
+    -- other formatters ...
+  }
+})
