0a49d4b9343f704f84cc984fa8ffcb96b24fc5eb
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-10 16:57:04 +0200

    Moved install dvim to be part of install.sh

--- install.sh
+++ install.sh
@@ -38,4 +38,8 @@ make && sudo make install
 cd ..
 
 # Install Neovim nightly build as DVIM
-. ~/scripts/install-dvim.sh
+curl -Lo dvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x dvim.appimage
+./dvim.appimage --appimage-extract
+sudo mv squashfs-root /dvim/
+sudo ln -s /dvim/AppRun /usr/bin/dvim
--- scripts/install-dvim.sh
+++
@@ -1,6 +0,0 @@
-cd ~/dotfiles/
-curl -Lo dvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x dvim.appimage
-./dvim.appimage --appimage-extract
-sudo mv squashfs-root /dvim/
-sudo ln -s /dvim/AppRun /usr/bin/dvim
