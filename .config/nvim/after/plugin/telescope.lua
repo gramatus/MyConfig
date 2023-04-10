7f245039d907a8b3b9c64462559159d5904d1767
Torgeir S. hos Sykehuspartner <torgst@sykehuspartner.no>
2023-04-10 19:17:08 +0200

    Added some more smart remaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,15 @@
 require('telescope').setup {
-	file_ignore_patterns = { "^./.git/"},
+    file_ignore_patterns = { "^./.git/" },
 }
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+if vim.g.vscode then
+    vim.keymap.set('n', '<leader>pf', "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
+else
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+end
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
--- .config/nvim/lua/torgst/remap.lua
+++ .config/nvim/lua/torgst/remap.lua
@@ -1,11 +1,17 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+if vim.g.vscode then
+    vim.keymap.set('n', '<leader>pv', "<Cmd>call VSCodeNotify('workbench.action.files.openFile')<CR>")
+else
+    vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+end
+
 
 vim.keymap.set("n", "<leader>cl", "<cmd>so ~/.config/nvim/after/plugin/colors.lua<CR>") -- For use while setting up my own colors
 vim.keymap.set("n", "<leader>ph", "<cmd>Telescope find_files hidden=true<cr>")
 
+-- vim.keymap.set("x", "cc", "<Cmd>call VSCodeNotifyVisual('editor.action.clipboardCopyAction', 1)<CR>")
+
 vim.keymap.set("n", "<leader>p", "<nop>")
--- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
 local lsp_formatting = function(bufnr)
     vim.lsp.buf.format({
         filter = function(client)
@@ -22,7 +28,8 @@ end
 vim.keymap.set("n", "<C-k><C-d>", lsp_formatting)
 -- TODO: Look at these suggestions: https://alpha2phi.medium.com/neovim-for-beginners-key-mappings-and-whichkey-31dbf58f9f87
 
-vim.keymap.set("n", "<leader>bg", ':exec &bg=="light"? "set bg=dark" : "set bg=light"<CR>', {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>bg", ':exec &bg=="light"? "set bg=dark" : "set bg=light"<CR>',
+    { noremap = true, silent = true })
 
 -- -- TESTING: Move "arrows" one key to the right
 -- vim.keymap.set("", "ø", "l")
@@ -34,8 +41,13 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+if vim.g.vscode then
+    vim.keymap.set("n", "<C-d>", "<C-d><Cmd>call VSCodeExtensionNotify('reveal', 'center', 0)<CR>")
+    vim.keymap.set("n", "<C-u>", "<C-u><Cmd>call VSCodeExtensionNotify('reveal', 'center', 0)<CR>")
+else
+    vim.keymap.set("n", "<C-d>", "<C-d>zz")
+    vim.keymap.set("n", "<C-u>", "<C-u>zz")
+end
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -50,10 +62,10 @@ end)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -73,10 +85,9 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/torgst/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
--- vim-commentary doesn't seem to add keymaps by itself (might be some interference from vscode-neovim), thus I try adding my own
-vim.keymap.set("n", "<leader>gc", "<Plug>Commentary")
-vim.keymap.set("n", "<leader>gcc", "<Plug>CommentaryLine")
-vim.keymap.set("x", "<leader>gc", "<Plug>Commentary")
+-- vim-commaentary doesn't seem to add keymaps by itself (or perhaps it will later), anyway I try adding my own
+vim.keymap.set("x", "C-'", "<cmd>Commentary<CR>")
+vim.keymap.set("x", "gc", "<cmd>Commentary<CR>")
 
 -- Switch buffer
 vim.keymap.set("n", "<S-h>", ":bprevious<CR>", default_opts)
