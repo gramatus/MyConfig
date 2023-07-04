82bf99eb9941a5e2a86c8c1290d21a674d840c8b
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-07-04 18:02:56 +0000

    Basic tailwind LSP setup

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -270,6 +270,24 @@ else
         filetypes = { 'terraform', 'tf' }
     }
 
+    lspconfig.tailwindcss.setup {
+
+        settings = {
+            tailwindCSS = {
+                classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+                lint = {
+                    cssConflict = "warning",
+                    invalidApply = "error",
+                    invalidConfigPath = "error",
+                    invalidScreen = "error",
+                    invalidTailwindDirective = "error",
+                    invalidVariant = "error",
+                    recommendedVariantOrder = "warning"
+                },
+                validate = true
+            }
+        }
+    }
     lsp.setup()
 
     vim.diagnostic.config({
