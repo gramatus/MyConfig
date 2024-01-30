ef47da5f50ea1c8c7b2e29342e838ae15cdeed5f
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2024-01-30 17:08:15 +0000

    Changed neovim to use kickstart config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -sr scripts ~/scripts
 if [ ! -d "~/.config" ]; then
     mkdir ~/.config;
 fi
-ln -sr .config/nvim ~/.config/nvim # Need to symlink every folder in .config
+ln -sr .config/nvim.kickstart ~/.config/nvim # Need to symlink every folder in .config
 rm ~/.zshrc
 ln -sr .zshrc ~/.zshrc
 ln -sr .tmux.conf ~/.tmux.conf
