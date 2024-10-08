76a3c6b6a8928d65c5642f5265d2342d2c00a1f9
Torgeir S. hos Sykehuspartner <93591857+torgst@users.noreply.github.com>
2024-10-08 15:23:14 +0200

    fix: add -y flag on installing node...

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ if ! which node; then
     NODE_MAJOR=20
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     sudo apt-get update
-    sudo apt-get install nodejs
+    sudo apt-get install -y nodejs
 fi
 
 if which code; then
