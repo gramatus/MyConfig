0023856676ba8a130fd7b247e7b2fd735226361a
Torgeir S. hos Sykehuspartner <93591857+torgst@users.noreply.github.com>
2025-03-27 23:31:30 +0100

    feat: add installation of GitHub CLI in install.sh

--- install.sh
+++ install.sh
@@ -71,6 +71,17 @@ if ! which node; then
     sudo apt-get install -y nodejs
 fi
 
+echo "############### Installing GitHub CLI ###############"
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+  && sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+  && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y
+
+
 if which code; then
   echo "############### Ensuring vs code extensions are installed (or at least try to ensure it) ###############"
   code --install-extension "donjayamanne.githistory"
